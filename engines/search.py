2fcce341830dda82e434278529e37ee474f5aec7
Samuel Walters-Nevet <matrix61312@gmail.com>
2014-12-11 22:28:55 -0500

    Searches through stocks & companies based upon a term and returns results.

---
+++ engines/search.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import sys
+import MySQLdb as mdb
+
+
+def connect():
+    return mdb.connect(host="localhost", user="root", passwd="toor", db="greed")
+
+
+def main(term):
+    results = ''
+    ret = ""
+
+    try:
+        con = connect()
+        cur = con.cursor()
+
+        stmt = """SELECT * FROM greed.stock
+                WHERE
+                    (symbol LIKE '%""" + term + """"%')
+                OR
+                    (company LIKE '%""" + term + """%');"""
+
+        cur.execute(stmt)
+        results = cur.fetchall()
+
+        for a_res in results:
+            ret = ret + a_res[0] + "," + a_res[1] + '\n'
+
+    except mdb.Error as e:
+        print(("Error %d: %s" % (e.args[0], e.args[1])))
+        sys.exit(1)
+
+    finally:
+        if con:
+            con.commit()
+            con.close()
+
+        return ret
+
+
+if __name__ == "__main__":
+    if(sys.argv[1:]):
+        term = sys.argv[1:][0]
+
+    print main(term)
+    sys.exit(1)
