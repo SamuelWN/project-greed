478d0f888b48c97b214e7ab28334c3492f44c4dd
Samuel Walters-Nevet <matrix61312@gmail.com>
2014-12-16 03:15:57 -0500

    Adds stock ticker id and company name to the stock database

---
+++ engines/stock_db_engine.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import MySQLdb as mdb
+import urllib2
+import time
+
+
+def connect():
+    return mdb.connect(host="localhost", user="root", passwd="toor", db="greed")
+
+
+def main():
+    i = 1
+    prefix = "http://finance.yahoo.com/d/quotes.csv?s="
+
+    stmt = """INSERT INTO greed.stock
+            (symbol, company) VALUES (\"%s\", \"%s\")
+            ON DUPLICATE KEY UPDATE symbol = symbol;"""
+
+    con = connect()
+    cur = con.cursor()
+
+    while i < 16:
+        url = prefix
+
+        s = open("./stocklists/stock" + str(i), "r")
+        for stock in s.readlines():
+            url += str(stock).strip() + '+'
+
+        url = url[:-1] + "&f=sn"
+
+        content = (urllib2.urlopen(url)).read()
+        splicelist = content.split("\n")
+
+        for quote in splicelist:
+            if quote:
+                val = quote.split("\",\"")
+                symb = (str(val[0])[1:]).strip()
+                comp = (str(val[1])[:-2]).strip()
+                cur.execute(stmt % (symb, comp))
+
+                con.commit()
+
+        i = i + 1
+
+# Re-aquire stock data every hour:
+    time.sleep(3600)
+    main()
+
+
+if __name__ == "__main__":
+    main()
